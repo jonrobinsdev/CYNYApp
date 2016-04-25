46f3591cb7cc371f4cd4f90a82ecfb286316516a
Jonathan Robins <jonrobns@gmail.com>
2016-04-25 11:02:28 -0400

    initial commit

---
+++ CYNY/SocialLoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SocialLoginViewController.swift
+//  CYNY
+//
+//  Created by Jonathan Robins on 4/23/16.
+//  Copyright © 2016 cyny. All rights reserved.
+//
+
+import UIKit
+
+class SocialLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SocialLoginButtonPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func SkipButtonPressed(sender: AnyObject) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
